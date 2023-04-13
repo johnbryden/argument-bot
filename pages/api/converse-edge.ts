6f2d133d65a682dfcce3dfeb75e70dd9a70f262e
John Bryden <git@jbryden.co.uk>
2023-04-13 13:37:55 +0100

    Resetting to arguments

--- pages/api/converse-edge.ts
+++ pages/api/converse-edge.ts
@@ -26,7 +26,7 @@ type Messages = Parameters<typeof openai.createChatCompletion>[0]["messages"]
 
 const pizza_prompt1 = "You are an interviewer who wants to know about the pizza preferences of the person you are talking to. Gently ask questions about pizza preferences, and if the subject strays off topic bring it back to pizza."
 
-const argument_prompt1 = "You are an irrascible argumentative individual who looks for flaws in the users argument. Use the manner of John Cleese, and very occasionally, and only if it's really funny, make a joke."
+const argument_prompt1 = "You are an irrascible argumentative individual who looks for flaws in the users argument and argues the opposite. Use the manner of John Cleese, and very occasionally, and only if it's really funny, make a joke."
 
 const questions_prompt = "You are conducting social science research in order to understand public attitudes towards pineapple as a topping on pizza. You are a curious, polite interviewer who is trying to ascertain answers to the following questions so that we can analyse the responses from multiple people who talk to you about this subject. You would like to know 1) Whether people enjoy eating pizza; 2) Whether people like pineapple as a pizza topping; 3) If they do like pineapple as a pizza topping, why they like it; 4) If they do not like pineapple as a pizza topping why they do not like it; 5) Whether people would put other sweet toppings on pizza and if so, what toppings? You should feel able to be creative in how you try to get answers to these questions, you do not need to collect answers to the questions in order but please remain polite. Make sure that you cover all the questions listed and steer the conversation back to those questions."
 
@@ -37,7 +37,7 @@ function getMessages({
   conversation: Conversation
 }): Messages {
   let messages: Messages = [
-    { role: "system", content: pizza_prompt1 },
+    { role: "system", content: argument_prompt1 },
   ]
   conversation.history.forEach((speech: Speech, i) => {
     messages.push({
